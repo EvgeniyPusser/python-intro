1160cfb3bc0b21dbdd619be5e01b3a827b274e33
Evgeniy Pusser <36860378+EvgeniyPusser@users.noreply.github.com>
2025-09-18 17:54:27 +0300

    смешная функция

--- hello.py
+++ hello.py
@@ -1,4 +1,28 @@
-x: int = 1
-x=2
-print(x)
-y = "hello"
+# x: int = 1
+# x=2
+# print(x)
+# y = "hello"
+def bSearchSortedList(arr, target):
+    # найдём левую границу: первый индекс, где arr[idx] >= target
+    left, right = 0, len(arr)
+    while left < right:
+        mid = (left + right) // 2
+        if arr[mid] >= target:
+            right = mid
+        else:
+            left = mid + 1
+
+    idx = left  # сюда можно вставить target и сохранить порядок
+    if idx < len(arr) and arr[idx] == target:
+        return idx              # это и есть первое вхождение
+    else:
+        return -(idx + 1) 
+      
+      # не найдено → код вставки
+arr = [1, 1, 1, 3, 4, 5, 11]
+
+print(bSearchSortedList(arr, 3))
+print(bSearchSortedList(arr, 6))
+print(bSearchSortedList(arr, 1))
+print(bSearchSortedList(arr, 0))
+print(bSearchSortedList(arr, 2))
